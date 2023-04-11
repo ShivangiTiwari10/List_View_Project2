82ce868ca3a77773141c754a51ee1df8308824b0
Shivangi <shivangit1111@gmail.com>
2023-04-11 13:54:38 +0530

    build: view Bind

--- app/src/main/java/com/example/listviewproject2/MainActivity.kt
+++ app/src/main/java/com/example/listviewproject2/MainActivity.kt
@@ -2,10 +2,14 @@ package com.example.listviewproject2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.listviewproject2.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
