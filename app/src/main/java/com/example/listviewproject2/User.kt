67fe2d3c8c104d0ca4b7b4f221ce7e7d0e1c3a14
Shivangi <shivangit1111@gmail.com>
2023-04-11 14:12:15 +0530

    feat: makeData class

---
+++ app/src/main/java/com/example/listviewproject2/User.kt
@@ -0,0 +1,7 @@
+package com.example.listviewproject2
+
+data class User(var name:String,
+                var lastMsg:String,
+                var lastMsgTime:String,
+                var phoneNo:String,
+                var imageId:Int)
