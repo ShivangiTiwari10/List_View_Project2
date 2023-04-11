b17e89948cba335489a0b2dbc227099d84171dc6
Shivangi <shivangit1111@gmail.com>
2023-04-11 14:27:17 +0530

    feat: create Data

--- app/src/main/java/com/example/listviewproject2/MainActivity.kt
+++ app/src/main/java/com/example/listviewproject2/MainActivity.kt
@@ -5,11 +5,20 @@ import android.os.Bundle
 import com.example.listviewproject2.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var userArrayList: ArrayList<User>
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        var name = arrayOf("Bhoomika", "Divya", "Disha", "Malyka","Swapna")
+        var lastMsg = arrayOf("Developer", "Docter", "Parler", "Shoap","Teacher")
+        var lastMsgTime = arrayOf("5:15" ,"2:5", "6", "9","8")
+        var phoneNo = arrayOf("25836974","159753456","846231479","987654321","9873298354")
+        var imageId = intArrayOf(R.drawable.image4,R.drawable.image4,R.drawable.image4,R.drawable.image4)
+
     }
 }
