d2cfc44dac38894ffedc62332154b1050cf24b0b
Shivangi <shivangit1111@gmail.com>
2023-04-11 14:46:48 +0530

    feat: add user in arrayList

--- app/src/main/java/com/example/listviewproject2/MainActivity.kt
+++ app/src/main/java/com/example/listviewproject2/MainActivity.kt
@@ -14,11 +14,20 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        var name = arrayOf("Bhoomika", "Divya", "Disha", "Malyka","Swapna")
-        var lastMsg = arrayOf("Developer", "Docter", "Parler", "Shoap","Teacher")
-        var lastMsgTime = arrayOf("5:15" ,"2:5", "6", "9","8")
-        var phoneNo = arrayOf("25836974","159753456","846231479","987654321","9873298354")
-        var imageId = intArrayOf(R.drawable.image4,R.drawable.image4,R.drawable.image4,R.drawable.image4)
+        val name = arrayOf("Bhoomika","Divya","Disha","Malyka","Swapna")
+        val lastMsg = arrayOf("Developer","Docter","Parler","Shoap","Teacher")
+        val lastMsgTime = arrayOf("5:15","2:5","6","9","8")
+        val phoneNo = arrayOf("25836974","159753456","846231479","987654321","9873298354")
+        val imageId = intArrayOf(R.drawable.image4,R.drawable.image4,R.drawable.image4,R.drawable.image4,R.drawable.image4)
 
+
+        userArrayList = ArrayList()
+
+        for (eachIndex in name.indices){
+
+            val user = User(name[eachIndex],lastMsg[eachIndex],lastMsgTime[eachIndex],phoneNo[eachIndex],imageId[eachIndex])
+
+            userArrayList.add(user)
+        }
     }
 }
