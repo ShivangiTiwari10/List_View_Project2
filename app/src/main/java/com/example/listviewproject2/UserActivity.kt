0f5112f548204db82d07403f328ab5abefeafee1
Shivangi <shivangit1111@gmail.com>
2023-04-12 11:08:46 +0530

    feat: view bind

--- app/src/main/java/com/example/listviewproject2/UserActivity.kt
+++ app/src/main/java/com/example/listviewproject2/UserActivity.kt
@@ -2,10 +2,15 @@ package com.example.listviewproject2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.listviewproject2.databinding.ActivityUserBinding
 
 class UserActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityUserBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_user)
+
+        binding = ActivityUserBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
