fafc48c371505668ff755a831d024a95910bc136
Shivangi <shivangit1111@gmail.com>
2023-04-12 13:43:42 +0530

    feat: get user detail

--- app/src/main/java/com/example/listviewproject2/UserActivity.kt
+++ app/src/main/java/com/example/listviewproject2/UserActivity.kt
@@ -1,5 +1,6 @@
 package com.example.listviewproject2
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.listviewproject2.databinding.ActivityUserBinding
@@ -7,10 +8,17 @@ import com.example.listviewproject2.databinding.ActivityUserBinding
 class UserActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityUserBinding
+
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityUserBinding.inflate(layoutInflater)
+
+        binding.name.text = intent.getStringExtra("name")
+        binding.phone.text = intent.getStringExtra("Phone")
+        binding.profileImage.id = intent.getIntExtra("Image",R.drawable.floura)
         setContentView(binding.root)
+
     }
 }
